668229abc64492c33969dc14768f58a54a8ed63f
LucasKeiti28 <lucaskeiti28@gmail.com>
2020-01-07 15:42:17 -0300

    Config One Signal.

--- App.js
+++ App.js
@@ -1,17 +1,44 @@
-import React from 'react';
-
+import React, {Component} from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
+
+import OneSignal from 'react-native-onesignal';
 
 // import { Container } from './styles';
 
 import Tokyo from './assets/tokyo.jpeg';
 
-const App = () => (
-  <View style={styles.container}>
-    <Text style={styles.text}> Test Deploy App 2020 v2 </Text>
-    <Image source={Tokyo} style={styles.image} />
-  </View>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    OneSignal.init('574588dd-ac60-4461-bfda-a7f5d10d9478');
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  onReceived = data => {};
+
+  onOpened = notification => {};
+
+  onIds = id => {};
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}> Test Deploy App 2020 v2 </Text>
+        <Image source={Tokyo} style={styles.image} />
+      </View>
+    );
+  }
+}
 
 export default App;
 
